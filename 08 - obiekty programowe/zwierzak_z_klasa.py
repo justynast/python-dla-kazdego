713f09f1fe90a80fb3dbac8b8f3535baa3ffcf55
Justyna Stępniak <stepniakj@gmail.com>
2017-07-26 12:56:12 +0200

    finished

--- 08 - obiekty programowe/zwierzak_z_klasa.py
+++ 08 - obiekty programowe/zwierzak_z_klasa.py
@@ -7,4 +7,24 @@ class Critter(object):
 
     @staticmethod
     def status():
-        print("Ogólna liczba zwierzaków wynosi:", Critter.total)
+        print("\nOgólna liczba zwierzaków wynosi:", Critter.total)
+
+    def __init__(self, name):
+        print("Urodził się zwierzak!")
+        self.name = name
+        Critter.total += 1
+
+# część główna
+
+print("Uzyskanie dostępu do atrybutu klasy Critter.total:", Critter.total)
+
+print("\nTworzenie zwierzaków")
+crit1 = Critter("zwierzak 1")
+crit2 = Critter("zwierzak 2")
+crit3 = Critter("zwierzak 3")
+
+Critter.status()
+
+print("Uzyskanie dostępu do atrybutu klasy poprzez obiekt:", crit1.total)
+
+input("Aby zakonczyc program nacisnij klawisz enter")
