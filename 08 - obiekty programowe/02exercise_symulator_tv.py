95a9314a42ea3dc3d85ca844d2a9129aba4ca677
Justyna Stępniak <stepniakj@gmail.com>
2017-07-28 15:48:59 +0200

    inital commit

---
+++ 08 - obiekty programowe/02exercise_symulator_tv.py
@@ -0,0 +1,5 @@
+"""
+Program, który symuluje telewizor, tworząc go jako obiekt.
+Użytkownik powinien mieć możliwość wprowadzenia numeru kanału oraz zwiększania/zmniejszania głośności.
+Zastosuj mechanizm zapewniający utrzymywanie numeru kanału we właściwych zakresach.
+"""
