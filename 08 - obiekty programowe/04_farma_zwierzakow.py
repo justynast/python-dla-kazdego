3c93294e671b17de8bf439f394c99c0812e4b3e2
Justyna Stępniak <stepniakj@gmail.com>
2017-08-18 14:03:37 +0200

    initial commit

---
+++ 08 - obiekty programowe/04_farma_zwierzakow.py
@@ -0,0 +1,8 @@
+"""
+Ćwiczenie 4
+Utwórz program Farma zwierzaków, konkretyzując kilka obiektów klasy Critter i śledząc ich stan poprzez listę.
+Naśladuj program Opiekun zwierzaka, lecz zamiast wymagać od użytkownika zajmowania się pojedynczym zwierzakiem,
+zażądaj od niego opieki nad całą farmą.
+Każda pozycja menu powinna umożliwić użytkownikowi wykonanie pewnej czynności obejmującej wszystkie zwierzaki.
+Aby program był ciekawszy, nadaj każdemu zwierzakowi wybrany losowo poziom głodu i nudy.
+"""
