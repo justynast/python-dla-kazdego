c6c9e4c4162d1a707b17c4f8ec632b2935582028
Justyna Stępniak <stepniakj@gmail.com>
2017-08-19 14:18:26 +0200

    finished

--- 09 - programowanie obiektowe/gra Blackjack/karty.py
+++ 09 - programowanie obiektowe/gra Blackjack/karty.py
@@ -68,3 +68,7 @@ class Deck(Hand):
                     self.give(top_card, hand)
                 else:
                     print("Nie mogę dalej rozdawać. Zabrakło kart!")
+
+if __name__ == "__main__":
+    print("To moduł zawierający klasy do gry w karty.")
+    input("\n\nAby zakończyć program, naciśnij klawisz Enter")
