afe3b3d1940f9153c2e1d4b827901e09135428ca
Justyna Stępniak <stepniakj@gmail.com>
2017-08-19 14:15:14 +0200

    hand class added

--- 09 - programowanie obiektowe/gra Blackjack/karty.py
+++ 09 - programowanie obiektowe/gra Blackjack/karty.py
@@ -21,3 +21,29 @@ class Card(object):
 
     def flip(self):
         self.is_face_up = not self.is_face_up
+
+
+class Hand(object):
+    """ Ręka - karty do gry w ręku gracza """
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        if self.cards:
+            rep = ""
+            for card in self.cards:
+                rep += str(card) + "\t"
+        else:
+            rep = "<pusta>"
+
+        return rep
+
+    def clear(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def give(self, card, other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
