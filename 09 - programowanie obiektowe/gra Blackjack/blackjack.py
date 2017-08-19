17f7c28ec8ef516f871a01a237d6feb468ad4bc0
Justyna Stępniak <stepniakj@gmail.com>
2017-08-19 14:29:54 +0200

    bjcard class added

--- 09 - programowanie obiektowe/gra Blackjack/blackjack.py
+++ 09 - programowanie obiektowe/gra Blackjack/blackjack.py
@@ -2,3 +2,17 @@
 # od 1 do 7 graczy współzawodniczy z rozdającym
 
 import karty, gry
+
+class BJ_Card(karty.Card):
+    """ karta do blackjacka """
+    ACE_VALUE = 1
+
+    @property
+    def value(self):
+        if self.is_face_up:
+            v = BJ_Card.RANKS.index(self.rank) + 1
+            if v > 10:
+                v = 10
+        else:
+            v = None
+        return v
