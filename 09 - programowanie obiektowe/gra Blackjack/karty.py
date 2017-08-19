34583c33e1a4e89793cd4e7028b401724090d5a5
Justyna Stępniak <stepniakj@gmail.com>
2017-08-19 14:13:27 +0200

    card class added

---
+++ 09 - programowanie obiektowe/gra Blackjack/karty.py
@@ -0,0 +1,23 @@
+# moduł karty
+# podstawowe klasy do gry w karty
+
+
+class Card(object):
+    """ Karta do gry """
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["c", "d", "h", "s"]
+
+    def __init__(self, rank, suit, face_up = True):
+        self.rank = rank
+        self.suit = suit
+        self.is_face_up = face_up
+
+    def __str__(self):
+        if self.is_face_up:
+            rep = self.rank + self.suit
+        else:
+            rep = "XX"
+        return rep
+
+    def flip(self):
+        self.is_face_up = not self.is_face_up
