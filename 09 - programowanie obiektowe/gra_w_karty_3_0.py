b4c7f3795e2e244ad4c4b231b36b56a3a50a2db9
Justyna Stępniak <stepniakj@gmail.com>
2017-08-19 13:35:14 +0200

    positionable card class added

--- 09 - programowanie obiektowe/gra_w_karty_3_0.py
+++ 09 - programowanie obiektowe/gra_w_karty_3_0.py
@@ -21,3 +21,19 @@ class Unprintable_Card(Card):
     def __str__(self):
         return "<utajniona>"
 
+
+class Positionable_Card(Card):
+    """ karta, która może być odkryta lub zakryta """
+    def __init__(self, rank, suit, face_up = True):
+        super(Positionable_Card, self).__init__(rank, suit)
+        self.is_face_up = face_up
+
+    def __str__(self):
+        if self.is_face_up:
+            rep = super(Positionable_Card, self).__str__()
+        else:
+            rep = "XX"
+        return rep
+
+    def flip(self):
+        self.is_face_up = not self.is_face_up
