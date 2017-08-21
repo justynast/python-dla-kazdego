d8496cbf2071c5f8cb019d68245cd8be6f7e118d
Justyna Stępniak <stepniakj@gmail.com>
2017-08-21 19:02:11 +0200

    class application

--- 10 - tworzenie interfejsów GUI/licznik_klikniec.py
+++ 10 - tworzenie interfejsów GUI/licznik_klikniec.py
@@ -2,3 +2,24 @@
 # Demonstruje powiązane zdarzenia z procedurą obsługi zdarzeń
 
 from tkinter import *
+
+class Application(Frame):
+    """ aplikacja z GUI, która zlicza kliknięcia przycisku"""
+    def __init__(self, master):
+        """ inicjalizuj ramkę """
+        super(Application, self).__init__(master)
+        self.grid()
+        self.bttn_clicks = 0 #liczba kliknięć przycisku
+        self.create_widget()
+
+    def create_widget(self):
+        """ utwórz przycisk który wyświetla liczbę kliknięć """
+        self.bttn = Button(self)
+        self.bttn["text"] = "Liczba kliknięć: 0"
+        self.bttn["command"] = self.update_count
+        self.bttn.grid()
+
+    def update_count(self):
+        """ zwiększ licznik kliknięć i wyświetl jego nową wartość """
+        self.bttn_clicks += 1
+        self.bttn["text"] = "Liczba kliknięć: " + str(self.bttn_clicks)
