18111ec5d43fa2885fcdb5922f9ca1a012f43107
Justyna Stępniak <stepniakj@gmail.com>
2017-08-21 12:28:29 +0200

    rozpoczęcie programu

---
+++ 10 - tworzenie interfejsów GUI/leniwe_przyciski.py
@@ -0,0 +1,13 @@
+# leniwe przyciski
+# demonstruje tworzenie przycisków
+
+from tkinter import *
+
+# utwórz okno główne
+root = Tk()
+root.title("Leniwe przyciski")
+root.geometry("200x85")
+
+# utwórz w oknie ramkę do pomieszczenia innych widżetów
+app = Frame(root)
+app.grid()
