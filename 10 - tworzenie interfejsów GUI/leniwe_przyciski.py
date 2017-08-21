752a39142ff468276f3638a05e04dcce6a43ad00
Justyna Stępniak <stepniakj@gmail.com>
2017-08-21 12:34:27 +0200

    utworzenie przycisków

--- 10 - tworzenie interfejsów GUI/leniwe_przyciski.py
+++ 10 - tworzenie interfejsów GUI/leniwe_przyciski.py
@@ -11,3 +11,17 @@ root.geometry("200x85")
 # utwórz w oknie ramkę do pomieszczenia innych widżetów
 app = Frame(root)
 app.grid()
+
+# utwórz w ramce przycisk
+bttn1 = Button(app, text = "Nic nie robię!")
+bttn1.grid()
+
+# utwórz w ramce drugi przycisk
+bttn2 = Button(app)
+bttn2.grid()
+bttn2.configure(text = "Ja również.")
+
+#utwórz w ramce trzeci przycisk
+bttn3 = Button(app)
+bttn3.grid()
+bttn3["text"] = "Takoż i ja."
