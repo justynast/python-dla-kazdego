8b5a8aaed1ae1f325ece5d238224e614c57cf86c
Justyna Stępniak <stepniakj@gmail.com>
2017-08-23 16:36:44 +0200

    tworzenie widżetu Entry

--- 10 - tworzenie interfejsów GUI/długowieczność.py
+++ 10 - tworzenie interfejsów GUI/długowieczność.py
@@ -16,6 +16,16 @@ class Application(Frame):
         # utwórz etykietę z instrukcją
         self.inst_lbl = Label(self, text = "Wprowadź hasło do sekretu długowieczności")
         self.inst_lbl.grid(row = 0, column = 0, columnspan = 2, sticky = W)
+
         # utwórz etykietę do hasła
         self.pw_lbl = Label(self, text = "Hasło: ")
         self.pw_lbl.grid(row = 1, column = 0, sticky = W)
+
+        # utwórz widżet Entry do przyjęcia hasła
+        self.pw_ent = Entry(self)
+        self.pw_ent.grid(row = 1, column = 1, sticky = W)
+
+        # utwórz przycisk 'Akceptuj'
+        self.submit_bttn = Button(self, text = "Akceptuj", command = self.reveal)
+        self.submit_bttn.grid(row = 2, column = 0, sticky = W)
+
