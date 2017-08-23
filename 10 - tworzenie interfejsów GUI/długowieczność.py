194343a297dbbdd2451316d5190547986e1ca324
Justyna Stępniak <stepniakj@gmail.com>
2017-08-23 16:32:54 +0200

    umiejscowienie widżetu za pomocą menedżera układu Grid

--- 10 - tworzenie interfejsów GUI/długowieczność.py
+++ 10 - tworzenie interfejsów GUI/długowieczność.py
@@ -2,3 +2,20 @@
 # demonstruje widżety text i entry oraz menedżer układu grid
 
 from tkinter import *
+
+class Application(Frame):
+    """ aplikacja z GUI, która może ujawnić sekret długowieczności"""
+    def __init__(self, master):
+        """ inicjalizuje ramkę """
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        """ utwórz widżety typu Button, Text i Entry"""
+        # utwórz etykietę z instrukcją
+        self.inst_lbl = Label(self, text = "Wprowadź hasło do sekretu długowieczności")
+        self.inst_lbl.grid(row = 0, column = 0, columnspan = 2, sticky = W)
+        # utwórz etykietę do hasła
+        self.pw_lbl = Label(self, text = "Hasło: ")
+        self.pw_lbl.grid(row = 1, column = 0, sticky = W)
