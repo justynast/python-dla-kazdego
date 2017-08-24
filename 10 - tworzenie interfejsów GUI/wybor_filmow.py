e802bdb6967643689d16eacc86aff0e74226f8f5
Justyna Stępniak <stepniakj@gmail.com>
2017-08-24 22:13:29 +0200

    funkcja create_widgets

--- 10 - tworzenie interfejsów GUI/wybor_filmow.py
+++ 10 - tworzenie interfejsów GUI/wybor_filmow.py
@@ -9,3 +9,36 @@ class Application(Frame):
         super(Application, self).__init__(master)
         self.grid()
         self.create_widgets()
+
+    def create_widgets(self):
+        """ utwórz widżety służące do wyboru gatunków filmów """
+        # utwórz etykietę z opisem
+        Label(self, text = "Wybierz swoje ulubione gatunki filmów.").grid(row = 0, column = 0, sticky = W)
+
+        # utwórz etykietę z instrukcją:
+        Label(self, text = "Zaznacz wszystkie, które chciałbyś wybrać:").grid(row = 1, column = 0, sticky = W)
+
+        # utwórz pole wyboru komedii
+        self.likes_comedy = BooleanVar()
+        Checkbutton(self, text = "komedia",
+                    variable = self.likes_comedy,
+                    command = self.update_text
+                    ).grid(row = 2, column = 0, sticky = W)
+
+        # utwórz pole wyboru dramatu
+        self.likes_drama = BooleanVar()
+        Checkbutton(self, text = "dramat",
+                    variable = self.likes_drama,
+                    command = self.update_text
+                    ).grid(row = 3, column = 0, sticky = W)
+
+        # utwórz pole wyboru romansu
+        self.likes_romance = BooleanVar()
+        Checkbutton(self, text = "romans",
+                    variable = self.likes_romance,
+                    command = self.update_text
+                    ).grid(row = 4, column = 0, sticky = W)
+
+        # utwórz pole tekstowe do wyświetlenia wyników
+        self.results_txt = Text(self, width = 40, height = 5, wrap = WORD)
+        self.results_txt.grid(row = 5, column = 0, columnspan = 3)
