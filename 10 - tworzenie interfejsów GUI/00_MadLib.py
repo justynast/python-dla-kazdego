c3128eab6ca077851af91719b7ff048909eef6d6
Justyna Stępniak <stepniakj@gmail.com>
2017-08-25 06:11:42 +0200

    konstruktor klasy Application

--- 10 - tworzenie interfejsów GUI/00_MadLib.py
+++ 10 - tworzenie interfejsów GUI/00_MadLib.py
@@ -2,3 +2,14 @@
 # Tworzy opowiadanie oparte na szczegółach wprowadzonych przez użytkownika
 
 from tkinter import *
+
+class Application(Frame):
+    """
+    Aplikacja z GUI, która tworzy opowiadanie oparte na danych
+    wprowadzonych przez użytkownika.
+    """
+    def __init__(self, master):
+        """ Inicjalizuj ramkę. """
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widgets()
