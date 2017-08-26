5ef776e8dcb9fcf6af8171a0b7dd7bf5a9e13e94
Justyna Stępniak <stepniakj@gmail.com>
2017-08-26 23:45:11 +0200

    funkcja create_widgets

--- 10 - tworzenie interfejsów GUI/00_MadLib.py
+++ 10 - tworzenie interfejsów GUI/00_MadLib.py
@@ -13,3 +13,19 @@ class Application(Frame):
         super(Application, self).__init__(master)
         self.grid()
         self.create_widgets()
+
+    def create_widgets(self):
+        """
+        Utwórz widżety potrzebne do pobrania informacji podanych
+        przez użytkownika i wyświetlenia opowiadania.
+        """
+        # utwórz etykietę z instrukcją
+        Label(self, text = "Wprowadź informacje do nowego opowiadania"
+              ).grid(row = 0, column = 0, columnspan = 2, sticky = W)
+
+        # utwórz etykietę i pole znakowe służące do wpisania imienia osoby
+        Label(self,
+              text = "Osoba: "
+              ).grid(row = 1, column = 1, sticky = W)
+        self.person_ent = Entry(self)
+        self.person_ent.grid(row = 1, column = 1, sticky = W)
