241b0fe0773ff691bb35286e31522a5e558522a1
Justyna Stępniak <stepniakj@gmail.com>
2017-09-07 13:56:40 +0200

    calculateTotal method

--- 10 - tworzenie interfejsów GUI/03exercise_zloz_zamowienie.py
+++ 10 - tworzenie interfejsów GUI/03exercise_zloz_zamowienie.py
@@ -22,25 +22,25 @@ class Application(Frame):
         # utwórz pola wyboru
         self.zupaCebulowa = BooleanVar()
         Checkbutton(self,
-                    text = "Zupa cebulowa",
+                    text = "Zupa cebulowa - 10zł",
                     variable = self.zupaCebulowa
                     ).grid(row = 1, column = 0, sticky = W)
 
         self.krazkiCebulowe = BooleanVar()
         Checkbutton(self,
-                    text = "Krążki cebulowe",
+                    text = "Krążki cebulowe - 5zł",
                     variable = self.krazkiCebulowe
                     ).grid(row = 2, column = 0, sticky = W)
 
         self.cebulaFaszerowana = BooleanVar()
         Checkbutton(self,
-                    text = "Cebula faszerowana",
+                    text = "Cebula faszerowana - 20zł",
                     variable = self.cebulaFaszerowana
                     ).grid(row = 3, column = 0, sticky = W)
 
         self.cebula = BooleanVar()
         Checkbutton(self,
-                    text = "Cebula",
+                    text = "Cebula - 1zł",
                     variable = self.cebula
                     ).grid(row = 4, column = 0, sticky = W)
 
@@ -56,4 +56,32 @@ class Application(Frame):
                              height = 2,
                              wrap = WORD
                              ).grid(row = 6, column = 0, columnspan = 2, sticky = W)
+
+    def calculateTotal(self):
+        """ oblicz rachunek na podstawie uzyskanych danych """
+
+        total = 0
+        if self.zupaCebulowa.get():
+            total += 10
+        if self.krazkiCebulowe.get():
+            total += 5
+        if self.cebulaFaszerowana.get():
+            total += 20
+        if self.cebula.get():
+            total += 1
+
+        txt = ""
+
+        if total == 0:
+            txt += "Nic nie zamówiłeś, nie masz nic do zapłaty."
+        else:
+            txt += "Twój rachunek wynosi: "
+            txt += str(total)
+            txt += " PLN."
+
+        # wyświetl dane
+        self.txtFrame.delete(0.0, END)
+        self.txtFrame.insert(0.0, txt)
+
+
 
