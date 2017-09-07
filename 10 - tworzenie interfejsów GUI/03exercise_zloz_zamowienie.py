86be664e26d970af0d01a2363907bb43021bb0fe
Justyna Stępniak <stepniakj@gmail.com>
2017-09-07 13:31:59 +0200

    initial commit

---
+++ 10 - tworzenie interfejsów GUI/03exercise_zloz_zamowienie.py
@@ -0,0 +1,6 @@
+"""
+Ćwiczenie 3
+Utwórz program z interfejsem GUI o nazwie "Złóż zamówienie", który przedstawia użytkownikowi
+proste restauracyjne menu, które wymienia pozycje i ich ceny. Pozwól użytkownikowi na wybranie różnych pozycji
+a potem pokaż końcowy rachunek.
+"""
