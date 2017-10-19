f67ada11e559420cb25c3c2ee60d3159debdd178
Justyna Stępniak <stepniakj@gmail.com>
2017-10-19 15:13:07 +0200

    screen background added

--- 11 - grafika/nowe_okno_graficzne.py
+++ 11 - grafika/nowe_okno_graficzne.py
@@ -5,4 +5,7 @@ from livewires import games
 
 games.init(screen_width=640, screen_height=480, fps=50)
 
+wall_image = games.load_image("kote.jpg", transparent=False)
+games.screen.background = wall_image
+
 games.screen.mainloop()
