27983231e03c8d37f157f86686257485b16018b5
Justyna Stępniak <stepniakj@gmail.com>
2017-10-19 15:35:13 +0200

    initial commit

---
+++ 11 - grafika/wysoki_wynik.py
@@ -0,0 +1,19 @@
+# demonstruje wyświetlanie tekstu na ekranie graficznym
+
+from livewires import games, color
+
+games.init(screen_width=640, screen_height=480, fps=50)
+
+wall_image = games.load_image("kote.jpg", transparent=False)
+games.screen.background = wall_image
+
+score = games.Text(value=1756521,
+                   size=60,
+                   color=color.pink,
+                   x=550,
+                   y=30)
+
+games.screen.add(score)
+
+
+games.screen.mainloop()
