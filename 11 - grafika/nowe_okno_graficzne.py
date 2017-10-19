73971354d5a68d1a591e186cc89bd4b8097a752a
Justyna Stępniak <stepniakj@gmail.com>
2017-10-19 15:24:15 +0200

    Sprite added

--- 11 - grafika/nowe_okno_graficzne.py
+++ 11 - grafika/nowe_okno_graficzne.py
@@ -8,4 +8,9 @@ games.init(screen_width=640, screen_height=480, fps=50)
 wall_image = games.load_image("kote.jpg", transparent=False)
 games.screen.background = wall_image
 
+heart_image = games.load_image("heart.gif")
+heart = games.Sprite(image=heart_image, x=520, y=140)
+games.screen.add(heart)
+
+
 games.screen.mainloop()
