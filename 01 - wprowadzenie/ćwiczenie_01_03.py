4bb5952fcbd28bd2c4a7acdb583ef40e2e911f59
Justyna Stępniak <stepniakj@gmail.com>
2017-11-09 16:14:22 +0100

    initial commit

---
+++ 01 - wprowadzenie/ćwiczenie_01_03.py
@@ -0,0 +1,6 @@
+# Napisz program, który wyświetla Twój ulubiony cytat. W kolejnym wierszu powinieneś podać nazwisko autora.
+
+print('"Ciąg za wajchę, Kronk!"')
+print('\t\t-Yzma')
+
+input("\n\nAby zakończyć naciśnij klawisz Enter.")
