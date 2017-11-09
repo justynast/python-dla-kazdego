7d880f2be105f8cff7335cf1caaeb2b2a27424c6
Justyna Stępniak <stepniakj@gmail.com>
2017-11-09 16:19:36 +0100

    initial commit

---
+++ 02 - typy, zmienne, operacje wejścia-wyjścia/ćwiczenie_02_02.py
@@ -0,0 +1,11 @@
+# Napisz program, który umożliwi użytkownikowi wprowadzenie nazw jego dwóch ulubionych przysmaków
+# Program powinien następnie wypisać nazwę nowego przysmaku utworzoną poprzez połączenie nazw podanych prez użytkownika.
+
+przysmak1 = input("Wprowadż nazwę pierwszego przysmaku: ")
+przysmak2 = input("Wprowadż nazwę drugiego przysmaku: ")
+
+przysmak3 = przysmak1 + przysmak2
+
+print("Twój trzeci przysmak to " + przysmak3 + ".")
+
+input("\n\nAby zakończyć naciśnij klawisz Enter.")
