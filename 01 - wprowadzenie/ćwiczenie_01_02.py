04cd1c75909eaed4b31eb4c7db63b2a9b64abf46
Justyna Stępniak <stepniakj@gmail.com>
2017-11-09 16:08:36 +0100

    initial commit

---
+++ 01 - wprowadzenie/ćwiczenie_01_02.py
@@ -0,0 +1,6 @@
+# Utwórz i zapisz program, który wyświetla Twoje imię
+# i (zanim zakończy swoje działanie) czeka, aż użytkownik naciśnie klawisz Enter.
+
+print("Justyna")
+
+input("\n\nAby zakończyć naciśnij klawisz Enter.")
